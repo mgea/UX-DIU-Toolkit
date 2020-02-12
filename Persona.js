a5d7d935c04301f196e42369dedc7464950e55b5
Miguel Gea <32359091+mgea@users.noreply.github.com>
2020-02-12 14:55:03 +0100

    Add files via upload
    UX Toolkit v1.0 (12/02/2020)

---
+++ Persona.js
@@ -0,0 +1,107 @@
+/*******************************************/
+/*             PERSONA.JS                  */
+/*     Datos para PERSONA TEMPLATE         */   
+/*          [DIU] UX Toolkit               */                        
+/*          ver 1.0, 2019                  */
+/*******************************************/
+    
+/****  README:       */
+/****  Modifica los datos para las Personas      */
+/****  Las imagenes para  'Photo'  están en carpeta ./photos **/
+/****  Si se usan nuevas imágenes se deben añadir a esa carpeta **/
+/****  Los valores de rating están entre 1..5 **/
+/****  recursos de imágenes:  https://www.vectorstock.com/royalty-free-vectors/vectors-by_zdeneksasek ***/
+
+
+
+angular.module("angular", [])
+	.controller("controller", ["$scope", function($scope) { 
+		
+		$scope.PersonaIndex = 0;
+		$scope.Personas = [
+			{		
+                
+                /*************************************/
+                /**** PRIMERA PERSONA          *******/
+                /*** Cambiar datos             *******/
+                /*************************************/
+                
+                
+				Id: 0,
+				Name: "Juan Lopez",
+				Photo: "man.png",
+				Quote: "Carpe Diem",
+				Age: 32,
+				Occupation: "Comercial en empresa de automocion",
+				Family: "Con pareja desde hace 1 año",
+				Location: "Granada (Huetor Vega)",
+				Character: "Le gusta disfrutar del tiempo",
+				PersonalityTraits: [
+					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 4 },
+					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 1 },
+					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 3 },
+					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 5 }
+				], 
+				Goals: ["Disfrutar del tiempo libre, viajar", "Cambiar a un trabajo más estable"],
+				Frustrations: ["le gusta la tecnología, pero siempre 'llama a un amigo' para resolver problemas", "Le gustaría tener más tiempo libre y leer más"],
+				Bio: "Es de Modril y vino a Granada para estudiar administración de empresas, pero no ha tenido grandes oportunidades de trabajo. LLeva 2 años contratado en la empresa actual. Aqui ha hecho buenos amigo en el trabajo y normalmente ser reunen para fiestas y a veces organizan viajes",
+				Tech: [
+					{ Name: "TIC/Internet", Value: 2 },
+					{ Name: "Movil", Value: 2 },
+					{ Name: "RRSS", Value: 3 },
+					{ Name: "Software", Value: 2 }
+					
+				], 
+                Contextos: "LLeva un tiempo preocupado y quiere desconectar Le gustaría dar una sorpresa a sus amigos para las vacaciones",  
+				PreferredChannels: [
+					{ Name: "Publicidad Tradicional", Value: 5 },
+					{ Name: "Online & Social Media", Value: 3 },
+					{ Name: "Recomendaciones & sugerencias", Value: 2 },
+					{ Name: "Persona confianza (amigos, boca a boca)", Value: 1 }
+				]
+			},
+			{	
+                
+                /*************************************/
+                /**** SEGUNDA PERSONA          *******/
+                /*** Cambiar datos             *******/
+                /*************************************/
+                
+                
+				Id: 1,
+				Name: "Monica Suarez",
+				Photo: "woman.png",
+				Quote: "A quotation that captures the essence of this person's personality",
+				Age: 17,
+				Occupation: "Searching for a cure for the Empress",
+				Family: "No parents, only family are the people who raised him.",
+				Location: "The Grassy Plains of Fantasia",
+				Character: "Strong, reliable and fearless.",
+				PersonalityTraits: [
+					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 3 },
+					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 3 },
+					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 2 },
+					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 2 }
+				], 
+				Goals: ["The goals this user hopes to achieve.", "A task that needs to be completed.", "A life goal to be reached.", "An experience to be felt."],
+				Frustrations: ["The frustrations this user would like to avoid.", "The obstacle that prevents the user from achieving their goals.", "The problems with the solutions already available.", "The product or service which does not currently exist."],
+				Bio: "The bio should be a short paragraph to describe the user journey. It should include some of their history leading up to a current use case. It may be helpful to incorporate information listed across the template and add pertinent details that may have been left out. Highlight factors of the user's personal and professional life that make this user an ideal customer of your product.",
+				Tech: [
+					{ Name: "TIC/Internet", Value: 5 },
+					{ Name: "Mobile", Value: 3 },
+					{ Name: "RRSS", Value: 3 },
+					{ Name: "Software", Value: 5 }
+					
+				], 
+                Contextos:   "The goals this user hopes to achieve." ,
+				PreferredChannels: [
+					{ Name: "Publicidad Tradicional (Ads)", Value: 5 },
+					{ Name: "Online & Social Media", Value: 2 },
+					{ Name: "Recomendaciones & sugerencias", Value: 2 },
+					{ Name: "Persona confianza (amigos, boca a boca)", Value: 2 }
+				]
+			}
+		];
+		$scope.model = $scope.Personas[0];
+
+	}])
--- README.md
+++ README.md
@@ -1,23 +1,15 @@
-# UX-DIU Toolkit
+# UX DIU Toolkit
 
-Recursos UX para DIU (en HTML5): 
-  - Persona Persona template (indexP.html) 
-  - User Journey map  (indexJ.html) 
-  
-Contenido: 
-  - Pensado para dos templates cada uno (de persona y journey map), las fichas están enlazadas por un componente Dropdown.
-  - El contenido está en los ficheros (Persona.js y Journey.js) que se pregargan con Angular.JS. Sólo hay que editar el contenido de estos archivos 
-  - Hay una serie de imagenes preseleccionadas en photos (para acciones)
-  
-  
-  
-  ![Persona Template](https://github.com/mgea/UX-DIU-Toolkit/blob/master/PersonaTemplate.png) 
-  
-  
-  ![JourneyMap](https://github.com/mgea/UX-DIU-Toolkit/blob/master/JourneyTemplate.png) 
+Herramientas en HTML5 para creación de: 
+    - 2 Personas 
+    - 2 User Journey Map 
 
-  - Se debe añadir información del equipo 
-  - Dos fichas (incluyendo imagen, se puede usar https://randomuser.me) 
+Recursos en: https://github.com/mgea/UX-DIU-Toolkit
+
+V.1.0 Dic 2019
+
+ToDo: 
+ - Configurar al movil
+ - E/S con JSON
 
 
-CCBY 2020
